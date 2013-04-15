5552266e4296f7a4da9742686c1d45cf8baba2b5
Marcel Stör <marcel@frightanic.com>
2013-04-15 23:04:41 +0200

    refactoring, enabling strict for JSLint

--- app.js
+++ app.js
@@ -13,215 +13,222 @@
 // DO NOT DELETE - this directive is required for Sencha Cmd packages to work.
 //@require @packageOverrides
 
+(function () {
+  "use strict";
+
 //<debug>
-Ext.Loader.setPath({
-  'Ext': 'touch/src',
-  'SunApp': 'app'
-});
+  Ext.Loader.setPath({
+    'Ext': 'touch/src',
+    'SunApp': 'app'
+  });
 //</debug>
 
-var allStationData;
-var tabPanel;
-var myLocationName;
-var myCoordinate;
+  var allStationData, tabPanel, myPlace, place;
 
-// http://www.movable-type.co.uk/scripts/latlong.html
-if (typeof(Number.prototype.toRad) === "undefined") {
-  Number.prototype.toRad = function () {
-    return this * Math.PI / 180;
+  // http://www.movable-type.co.uk/scripts/latlong.html
+  if (Number.prototype.toRad === undefined) {
+    Number.prototype.toRad = function () {
+      return this * Math.PI / 180;
+    };
   }
-}
-var coordinate = function (lat, lon) {
-  var R = 6371; // km
-  return {
-    distanceTo: function (other) {
-      var dLat = (other.lat - lat).toRad();
-      var dLon = (other.lon - lon).toRad();
-      var lat1 = lat.toRad();
-      var lat2 = other.lat.toRad();
+  place = function (lat, lon, name) {
+    var R = 6371; // km
+    return {
+      distanceTo: function (other) {
+        var dLat, dLon, lat1, lat2, a, c;
+        dLat = (other.lat - lat).toRad();
+        dLon = (other.lon - lon).toRad();
+        lat1 = lat.toRad();
+        lat2 = other.lat.toRad();
 
-      var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-      return Math.round(R * c * 1000) / 1000; // distance
-    },
-    lat: lat,
-    lon: lon
+        a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+          Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+        c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return Math.round(R * c * 10) / 10; // distance
+      },
+      setName: function (newname) {
+        this.name = newname;
+      },
+      lat: lat,
+      lon: lon,
+      name: name
+    };
   };
-};
 
-Ext.define('StationData', {
-  extend: 'Ext.data.Model',
-  config: {
-    fields: [
-      {name: 'wmo', type: 'int'},
-      {name: 'name', type: 'string'},
-      {name: 'lat', type: 'float'} ,
-      {name: 'lon', type: 'float'},
-      {name: 'ss', type: 'int'},
-      {name: 'sunLevel', type: 'int'},
-      {name: 'distance', type: 'float', defaultValue: '0.0'}
-    ]
-  }
-});
-
-allStationData = new Array();
-
-tabPanel = Ext.create("Ext.tab.Panel", {
-  requires: [
-    'Ext.TitleBar'
-  ],
-  fullscreen: true,
-  tabBarPosition: 'bottom',
-  items: [
-    {
-      docked: 'top',
-      xtype: 'titlebar',
-      title: 'SunApp'
-    },
-    {
-      title: 'Home',
-      iconCls: 'brightness1',
-      cls: 'home',
-      html: [
-        "<h1>Wait, there's more....</h1>",
-        '<p>Stay tuned while we do some number crunching in the ',
-        'background for you.</p>'
-      ].join("")
+  Ext.define('StationData', {
+    extend: 'Ext.data.Model',
+    config: {
+      fields: [
+        {name: 'wmo', type: 'int'},
+        {name: 'name', type: 'string'},
+        {name: 'lat', type: 'float'},
+        {name: 'lon', type: 'float'},
+        {name: 'ss', type: 'int'},
+        {name: 'sunLevel', type: 'int'},
+        {name: 'distance', type: 'float', defaultValue: '0.0'}
+      ]
     }
-  ]
-});
+  });
 
-Ext.application({
-  name: 'SunApp',
+  allStationData = [];
 
-  requires: [
-    'Ext.MessageBox'
-  ],
-
-  icon: {
-    57: 'resources/images/glasses.svg'
-  },
-  launch: function () {
-    var stationStore = Ext.create('Ext.data.Store', {
-      model: 'StationData',
-      autoLoad: true,
-      proxy: {
-        type: 'ajax',
-        url: 'data.json',
-        reader: {
-          type: 'json'
-        }
+  tabPanel = Ext.create("Ext.tab.Panel", {
+    requires: [
+      'Ext.TitleBar'
+    ],
+    fullscreen: true,
+    tabBarPosition: 'bottom',
+    items: [
+      {
+        docked: 'top',
+        xtype: 'titlebar',
+        title: 'SunApp'
+      },
+      {
+        title: 'Home',
+        iconCls: 'brightness1',
+        cls: 'home',
+        html: [
+          "<h1>Wait, there's more....</h1>",
+          '<p>Stay tuned while we do some number crunching in the ',
+          'background for you.</p>'
+        ].join("")
       }
-    });
-    stationStore.on('load', function (storeRef, records, successful) {
-      allStationData = records;
-    }, this);
-    var stationItemTemplate = new Ext.XTemplate(
-      '<tpl for=".">',
-      '<div class="stationList sunLevel{sunLevel}">{name}: <span class="distance">{distance}km</span></div>',
-      '</tpl>'
-    );
+    ]
+  });
 
-    Ext.create('Ext.util.Geolocation', {
-      requires: [
-        'Ext.TitleBar'
-      ],
-      autoUpdate: false,
-      listeners: {
-        locationupdate: function (geo) {
-          var stationList;
-          myCoordinate = coordinate(geo.getLatitude(), geo.getLongitude())
+  Ext.application({
+    name: 'SunApp',
 
-          Ext.Ajax.request({
-            url: '../transport/api.php/v1/locations',
-            method: 'GET',
-            params: {
-              x: myCoordinate.lat,
-              y: myCoordinate.lon
-            },
-            success: function (response) {
-              var text = response.responseText;
-              myLocationName = Ext.JSON.decode(text).stations[0].name;
-              Ext.Msg.show({
-                title: myLocationName,
-                msg: 'We believe your nearest station is ' + myLocationName,
-                buttons: Ext.Msg.OK,
-                icon: Ext.Msg.INFO
-              });
-            }
-          });
+    requires: [
+      'Ext.MessageBox'
+    ],
 
-          stationStore.filterBy(function (record, id) {
-            var sunLevel;
-            var sunshineMinutes = record.get('ss');
+    icon: {
+      57: 'resources/images/glasses.svg'
+    },
+    launch: function () {
+      var stationStore, stationItemTemplate;
+      stationStore = Ext.create('Ext.data.Store', {
+        model: 'StationData',
+        autoLoad: true,
+        proxy: {
+          type: 'ajax',
+          url: 'data.json',
+          reader: {
+            type: 'json'
+          }
+        }
+      });
+      stationStore.on('load', function (storeRef, records, successful) {
+        allStationData = records;
+      }, this);
+      stationItemTemplate = new Ext.XTemplate(
+        '<tpl for=".">',
+        '<div class="stationList sunLevel{sunLevel}">{name}: <span class="distance">{distance}km</span></div>',
+        '</tpl>'
+      );
 
-            if (sunshineMinutes < 5) {         // 0-4
-              sunLevel = 0;
-            } else if (sunshineMinutes < 50) { // 5 - 49
-              sunLevel = 1;
-            } else if (sunshineMinutes < 60) { // 50 - 59
-              sunLevel = 2;
-            } else {                          // 60
-              sunLevel = 3;
-            }
+      Ext.create('Ext.util.Geolocation', {
+        requires: [
+          'Ext.TitleBar'
+        ],
+        autoUpdate: false,
+        listeners: {
+          locationupdate: function (geo) {
+            var stationList;
+            myPlace = place(geo.getLatitude(), geo.getLongitude());
 
-            record.data.sunLevel = sunLevel;
-
-            if (sunLevel >= 2) {
-              if (record.get('distance') === 0.0) {
-                var recordCoordinate = coordinate(record.get('lat'), record.get('lon'));
-                record.data.distance = recordCoordinate.distanceTo(myCoordinate);
+            Ext.Ajax.request({
+              url: '../transport/api.php/v1/locations',
+              method: 'GET',
+              params: {
+                x: myPlace.lat,
+                y: myPlace.lon
+              },
+              success: function (response) {
+                var station = Ext.JSON.decode(response.responseText).stations[0];
+                myPlace.setName(station.name);
+                Ext.Msg.show({
+                  title: myPlace.name,
+                  msg: 'We believe your nearest station is ' + myPlace.name,
+                  buttons: Ext.Msg.OK,
+                  icon: Ext.Msg.INFO
+                });
               }
-              return true;
-            } else {
-              return false;
-            }
-          });
-          stationStore.sort([
-            {
-              property: 'distance',
-              direction: 'ASC'
-            }
-          ]);
-          stationList = Ext.create('Ext.dataview.List', {
-            title: 'Station List',
-            iconCls: 'brightness1',
-            store: stationStore,
-            itemTpl: stationItemTemplate
-          });
-          tabPanel.setItems([
-            {
-              docked: 'top',
-              xtype: 'titlebar',
-              title: 'Bern'              },
-            stationList
-          ]
-          );
-        },
-        locationerror: function (geo, bTimeout, bPermissionDenied, bLocationUnavailable, message) {
-          tabPanel.setItems([
-            {
-              docked: 'top',
-              xtype: 'titlebar',
-              title: 'Sorry'
-            },
-            {
-              title: 'Home',
+            });
+
+            stationStore.filterBy(function (record, id) {
+              var sunLevel, sunshineMinutes, recordPlace;
+              sunshineMinutes = record.get('ss');
+
+              if (sunshineMinutes < 5) {         // 0-4
+                sunLevel = 0;
+              } else if (sunshineMinutes < 50) { // 5 - 49
+                sunLevel = 1;
+              } else if (sunshineMinutes < 60) { // 50 - 59
+                sunLevel = 2;
+              } else {                          // 60
+                sunLevel = 3;
+              }
+
+              record.data.sunLevel = sunLevel;
+
+              if (sunLevel >= 2) {
+                if (record.get('distance') === 0.0) {
+                  recordPlace = place(record.get('lat'), record.get('lon'));
+                  record.data.distance = recordPlace.distanceTo(myPlace);
+                }
+                return true;
+              } else {
+                return false;
+              }
+            });
+            stationStore.sort([
+              {
+                property: 'distance',
+                direction: 'ASC'
+              }
+            ]);
+            stationList = Ext.create('Ext.dataview.List', {
+              title: 'Station List',
               iconCls: 'brightness1',
-              cls: 'home',
-              html: [
-                '<img width="12.5%" src="http://staging.sencha.com/img/sencha.png" />',
-                "<h1>Ouchh!</h1>",
-                "<p>We're really sorry but your device didn't ",
-                'allow us to detect your location or it failed ',
-                'to do so. Pity.</p>'
-              ].join("")
-            }
-          ]);
+              store: stationStore,
+              itemTpl: stationItemTemplate
+            });
+            tabPanel.setItems([
+              {
+                docked: 'top',
+                xtype: 'titlebar',
+                title: 'Bern'
+              },
+              stationList
+            ]);
+          },
+          locationerror: function (geo, bTimeout, bPermissionDenied, bLocationUnavailable, message) {
+            tabPanel.setItems([
+              {
+                docked: 'top',
+                xtype: 'titlebar',
+                title: 'Sorry'
+              },
+              {
+                title: 'Home',
+                iconCls: 'brightness1',
+                cls: 'home',
+                html: [
+                  '<img width="12.5%" src="http://staging.sencha.com/img/sencha.png" />',
+                  "<h1>Ouchh!</h1>",
+                  "<p>We're really sorry but your device didn't ",
+                  'allow us to detect your location or it failed ',
+                  'to do so. Pity.</p>'
+                ].join("")
+              }
+            ]);
+          }
         }
-      }
-    }).updateLocation();
-    tabPanel.show();
-  }
-});
+      }).updateLocation();
+      tabPanel.show();
+    }
+  });
+}());
