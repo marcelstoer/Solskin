2b4f7b27c9bf8e84004ac63ac41f02927419b98b
Marcel Stör <marcel@frightanic.com>
2013-10-13 23:08:30 +0200

    Produced and re-organized all app icons (all variations included).

--- app.js
+++ app.js
@@ -181,10 +181,10 @@ Ext.application({
   controllers: ['Application'],
 
   icon: {
-    '57': 'resources/icons/Icon.png',
-    '72': 'resources/icons/Icon~ipad.png',
-    '114': 'resources/icons/Icon@2x.png',
-    '144': 'resources/icons/Icon~ipad@2x.png'
+    '57': 'resources/icons/iOS/Icon.png',
+    '72': 'resources/icons/iOS/Icon-72.png',
+    '114': 'resources/icons/iOS/Icon@2x.png',
+    '144': 'resources/icons/iOS/Icon-72@2x.png'
   },
 
   isIconPrecomposed: true,
